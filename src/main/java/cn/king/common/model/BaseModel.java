fc489a22fa0a3eed9df17f73d8380bb03d2c83ef
lingunagbo <52281693+lingunagbo@users.noreply.github.com>
2019-06-27 22:19:20 +0800

    Update BaseModel.java

--- src/main/java/cn/king/common/model/BaseModel.java
+++ src/main/java/cn/king/common/model/BaseModel.java
@@ -1,5 +1,5 @@
 package cn.king.common.model;
-
+                                                                        
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
