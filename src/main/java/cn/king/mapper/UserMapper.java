7a0bc46b4b3da5f7c8afab67dd48afa80d765e0a
lingunagbo <52281693+lingunagbo@users.noreply.github.com>
2019-06-27 22:24:20 +0800

    Update UserMapper.java

--- src/main/java/cn/king/mapper/UserMapper.java
+++ src/main/java/cn/king/mapper/UserMapper.java
@@ -1,5 +1,5 @@
 package cn.king.mapper;
-                                                                      
+                                                                                                  
 import cn.king.entities.User;
 import org.apache.ibatis.annotations.Param;
 
